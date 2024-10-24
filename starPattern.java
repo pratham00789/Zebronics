10c4e2b0e4df78451517d974a5538d3ca55a6dde
Pratham <119481020+pratham00789@users.noreply.github.com>
2024-10-24 23:27:57 +0530

    Update starPattern.java

--- starPattern.java
+++ starPattern.java
@@ -1,5 +1,6 @@
 public class starPattern {
     public static void main(String[] args) {
         System.out.println("hello pratham how are you");
+        System.out.println("hello pratham kamble how are you again");
     }
 }
